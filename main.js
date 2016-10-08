ad6e997c90115558a4b697767fcbd483e65dcf23
Ben Shepheard <ben@codeagency.com>
2016-10-08 18:42:23 +0100

    Add hardcoded 'obstacle'

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function isIntersecting(r1, r2) {
+  return !(
+    r2.x > (r1.x + r1.width) ||
+    (r2.x + r2.width) < r1.x ||
+    r2.y > (r1.y + r1.height) ||
+    (r2.y + r2.height) < r1.y
+  );
+}
+
 $(function () {
 
   PIXI.loader.add('avatar', 'images/survivor-idle_rifle_0.png');
@@ -45,20 +54,19 @@ $(function () {
     // One projectile every 10 frames
     var fireRate = 10;
 
-    // This creates a texture from a 'bunny.png' image.
     var avatar = new PIXI.Sprite(resources.avatar.texture);
-    var bullets = [];
-
-    // Setup the position and scale of the avatar
     avatar.position.x = 400;
     avatar.position.y = 300;
-
     avatar.scale.x = 0.5;
     avatar.scale.y = 0.5;
     avatar.rotation = -Math.PI / 2;
-
-    // Add the avatar to the scene we are building.
     stage.addChild(avatar);
+
+    var obstacles = [];
+    var lastObstacle = Date.now();
+    var obstacleRate = 3000;
+
+    var bullets = [];
 
     // kick off the animation loop (defined below)
     animate();
@@ -72,6 +80,8 @@ $(function () {
       // Make the avatar follow the cursor
       avatar.position.x = cursorPosition.x - avatar.getBounds().width / 2 - 20;
       avatar.position.y = cursorPosition.y + avatar.getBounds().height / 2;
+
+
 
       if (isMouseDown) {
         if (firingFrameCount % fireRate == 0) {
@@ -87,6 +97,20 @@ $(function () {
         firingFrameCount++;
       }
 
+      // Generate a new obstacle every so often
+      if (Date.now() - lastObstacle > obstacleRate) {
+        var obstacle = new PIXI.Text('@RandomUser', {
+          font: '35px Arial',
+          align: 'center',
+          backgroundColor: 'black'
+        });
+        obstacle.position.x = 0;
+        obstacle.position.y = -obstacle.getBounds().height;
+        stage.addChild(obstacle);
+        obstacles.push(obstacle);
+        lastObstacle = Date.now();
+      }
+
       for (var i = bullets.length - 1; i >= 0; i--) {
         var bullet = bullets[i];
         bullet.position.y -= 10;
@@ -94,6 +118,20 @@ $(function () {
         if (bullet.position.y < -bullet.getBounds().height) {
           partContainer.removeChild(bullet);
           bullets.splice(i, 1);
+        }
+      }
+
+      for (var i = obstacles.length - 1; i >= 0; i--) {
+        var obstacle = obstacles[i];
+        obstacle.position.y += 4;
+
+        if (obstacle.position.y > window.innerHeiht + obstacle.getBounds().height) {
+          stage.removeChild(obstacle);
+          obstacles.splice(i, 1);
+        }
+
+        if (isIntersecting(avatar.getBounds(), obstacle.getBounds())) {
+          console.log('intersect');
         }
       }
 
