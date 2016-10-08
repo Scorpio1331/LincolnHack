f0bec0157210cda9b5198a7965eae65c205aa3e5
Luke Butterworth <butterworthld@gmail.com>
2016-10-08 23:44:12 +0100

    Commented Server code

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -1,11 +1,12 @@
 var http = require('http');
+//Get enviroment port, else use 1337
 var port = process.env.port || 1337;
 var fs = require('fs');
 var url = require('url');
-    
 
+//Create server
 http.createServer(function (req, res) {
-
+  //Allow control to stop getting access control origin errors
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
@@ -15,21 +16,21 @@ http.createServer(function (req, res) {
         res.end();
         return;
     }
-
+//Get path of webpage the requesting webpage asked for
     var path = url.parse(req.url).pathname;
-    if (path == "/gettweets") {
+    if (path == "/gettweets") { //If its gettweets, receive tweets
         res.writeHead(200, { "Content-Type": "text/JSON" });
         //Callback functions
         var error = function (err, res, body) {
             console.log('ERROR [%s]', err.message);
         };
         var success = function (data) {
-            
-            res.end(data);
+
+            res.end(data); //Send data as response
         };
 
-        var Twitter = require('twitter-node-client').Twitter;
-
+        var Twitter = require('twitter-node-client').Twitter; //Create handle for twitter api
+        //Set twitter connection config
         var config = {
             "consumerKey": "gwh4Oc5Tnwnx8Iwdq9aP1ltAJ",
 
@@ -42,22 +43,22 @@ http.createServer(function (req, res) {
         };
 
         var twitter = new Twitter(config);
+        //Search for tweets
+        twitter.getSearch({ 'q': '@realDonaldTrump','count': 50}, error, success);
 
-        twitter.getSearch({ 'q': '@realDonaldTrump','count': 100}, error, success);
-        
-    } else {
+    } else { //If file doesnt end in gettweets send error response
         fs.readFile('./index.html', function (err, file) {
             if (err) {
-                // write an error response or nothing here  
+                // write an error response or nothing here
                 return;
             }
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(file, "utf-8");
         });
     }
-   
 
 
-   
-}).listen(port);
-console.log("Server started!");
+
+
+}).listen(port); //Open port for listening
+console.log("Server started!"); //Log to console that server has started
