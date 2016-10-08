87253fd4c0016be11f4d790d26772b6e60a86572
Luke Butterworth <butterworthld@gmail.com>
2016-10-08 21:33:02 +0100

    fixed console logging

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -5,7 +5,7 @@ var url = require('url');
     
 
 http.createServer(function (req, res) {
-console.write("Server started!");
+console.log("Server started!");
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
